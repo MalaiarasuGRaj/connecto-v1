2befe7d5a935b7f0362e4bf938efd34fda529dd5
Malaiarasu G <govindarajmalaiarasu@gmail.com>
2025-08-19 06:40:01 +0000

    hi I'm sorry, but I don't have any company information available right n

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -22,6 +23,11 @@ const nextConfig: NextConfig = {
     allowedDevOrigins: [
       '*.cluster-ikxjzjhlifcwuroomfkjrx437g.cloudworkstations.dev',
     ],
+  },
+  experimental: {
+    outputFileTracingIncludes: {
+      '/*': ['./data/**/*'],
+    },
   },
 };
 
